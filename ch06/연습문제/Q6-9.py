45a90525a0e3ab3dee3481e3be084fbcd9056d62
LEE HANBIN <hanbin@kakao.com>
2021-06-26 18:34:20 +0900

    update Q6-9.py

--- ch06/연습문제/Q6-9.py
+++ ch06/연습문제/Q6-9.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 def bar1(value):
-    global image1, image2, image3, image, v1, v2
+    global image1, image2, image3, v1, v2
     v1 = value
     image3 = cv2.addWeighted(image1, v1/100, image2, v2/100, 0)
     image = cv2.hconcat([image1, image3, image2])
     cv2.imshow("dst", image)
 
 def bar2(value):
-    global image1, image2, image3, image, v1, v2
+    global image1, image2, image3, v1, v2
     v2 = value
     image3 = cv2.addWeighted(image1, v1/100, image2, v2/100, 0)
     image = cv2.hconcat([image1, image3, image2])
